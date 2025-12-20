eb5d387f492652c325c300924c68a117c8a1d5d5
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2025-12-20 17:01:55 +0300

    Fix requirements for jaxlib==0.4.23

--- transdreamerv3_8/dreamerv3/embodied/core/logger.py
+++ transdreamerv3_8/dreamerv3/embodied/core/logger.py
@@ -5,6 +5,7 @@ import json
 import os
 import re
 import time
+import uuid
 
 import numpy as np
 
@@ -267,6 +268,68 @@ class WandBOutput:
 
     for step, metrics in bystep.items():
       self._wandb.log(metrics, step=step)
+
+
+class CometOutput:
+
+  def __init__(self, pattern, logdir, config):
+    self._pattern = re.compile(pattern)
+    self._fps = 15
+    import comet_ml
+
+    run_id = os.getenv('COMET_RUN_ID', None)
+    mode = 'create'
+    if run_id is not None:
+      mode = 'get'
+
+    experiment = comet_ml.start(experiment_key=run_id, mode=mode)
+    experiment.log_parameters(dict(config))
+    experiment.set_name(os.getenv('COMET_EXPERIMENT_NAME', logdir.name))
+    self._experiment = experiment
+
+  def __call__(self, summaries):
+    bystep = collections.defaultdict(dict)
+    experiment = self._experiment
+    for step, name, value in summaries:
+      if len(value.shape) == 0 and self._pattern.search(name):
+        bystep[step][name] = float(value)
+      elif len(value.shape) == 1:
+        experiment.log_histogram_3d(value, name=name, step=step)
+      elif len(value.shape) == 2:
+        value = np.clip(255 * value, 0, 255).astype(np.uint8)
+        value = value[..., np.newaxis]
+        value = np.transpose(value, [2, 0, 1])
+        experiment.log_image(value, image_channels="first", name=name, step=step)
+      elif len(value.shape) == 3:
+        value = np.clip(255 * value, 0, 255).astype(np.uint8)
+        value = np.transpose(value, [2, 0, 1])
+        experiment.log_image(value, image_channels="first", name=name, step=step)
+      elif len(value.shape) == 4:
+        from moviepy.editor import ImageSequenceClip
+        # Sanity check that the channel dimension is the last
+        assert value.shape[3] in [1, 3, 4], f"Invalid shape: {value.shape}"
+        if value.shape[3] == 1:
+            value = np.repeat(value, 3, axis=3)
+        elif value.shape[3] == 4:
+            value = value[..., :3]
+        # If the video is a float, convert it to uint8
+        if np.issubdtype(value.dtype, np.floating):
+          value = np.clip(255 * value, 0, 255).astype(np.uint8)
+        value = list(value)
+        clip = ImageSequenceClip(value, fps=self._fps)
+        path = f'/tmp/{uuid.uuid4()}.mp4'
+        need_remove = False
+        try:
+          clip.write_videofile(path)
+          need_remove = True
+          experiment.log_video(path, name=name, step=step)
+        finally:
+          if need_remove:
+            os.remove(path)
+
+    for step, metrics in bystep.items():
+      metrics['global_step'] = step
+      self._experiment.log_metrics(metrics, step=step)
 
 
 class MLFlowOutput:
